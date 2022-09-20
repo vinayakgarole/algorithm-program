ae79681acac1754f3ca3501a93089b6a7adaa9ee
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-20 17:35:31 +0530

    added: Question to find your number

---
+++ src/com/bridgelabz/QuestionToFindYourNumber.java
@@ -0,0 +1,54 @@
+package com.bridgelabz;
+
+import java.util.*;
+
+class Guessno {
+    public int number;
+    public int inputNumber;
+    public int noOfGuesses = 0;
+
+    public int getNoOfGuesses() {
+        return noOfGuesses;
+    }
+
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
+    }
+
+    Guessno() {
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
+    }
+
+    void takeUserInput() {
+        System.out.println("Guess the Number:");
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
+    }
+
+    boolean isCorrectNumber() {
+        noOfGuesses++;
+        if (inputNumber == number) {
+            System.out.format("Yes you guessed it right, it was %d\nYou guessed it in %d attempts", number, noOfGuesses);
+            return true;
+        } else if (inputNumber < number) {
+            System.out.println("Too low...");
+        } else if (inputNumber > number) {
+            System.out.println("Too high...");
+        }
+        return false;
+    }
+
+}
+
+public class QuestionToFindYourNumber {
+    public static void main(String[] args) {
+        Guessno g = new Guessno();
+        boolean b = false;
+        while (!b) {
+            g.takeUserInput();
+            b = g.isCorrectNumber();
+        }
+
+    }
+}
